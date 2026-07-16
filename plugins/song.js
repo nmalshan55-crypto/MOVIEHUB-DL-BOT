d58e7e939184eac34f3ed118c1601c11e4f07897
Shan-droid770 <kumaradissanayaka30@gmail.com>
2026-07-16 15:17:07 +0530

    Refactor audio download to use ytdl directly

--- plugins/song.js
+++ plugins/song.js
@@ -1,5 +1,5 @@
 const {cmd , commands} = require('../command')
-const fg = require('@distube/ytdl-core')
+const ytdl = require('@distube/ytdl-core')
 const yts = require('yt-search')
 
 cmd({
@@ -13,6 +13,7 @@ try{
  if(!q) return reply("please give me url or title")
  const search = await yts(q);
  const data = search.videos[0];
+ if (!data) return reply("Couldn't find that song.")
  const url = data.url
 
 let desc = `
@@ -27,14 +28,16 @@ let desc = `
     MADE BY SAHAN DISSANAYAKA 💚
     `
     await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc}, {quoted:mek});
-   //download audio
 
-   let down = await fg.yta(url)
-   let downloadUrl = down.dl_url
+    //download audio directly from YouTube (no third-party API middleman)
+    const stream = ytdl(url, { filter: 'audioonly', quality: 'highestaudio' })
+    const chunks = []
+    for await (const chunk of stream) chunks.push(chunk)
+    const audioBuffer = Buffer.concat(chunks)
 
     //send audio + document message
-    await conn.sendMessage(from,{audio:{url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
-    await conn.sendMessage(from,{document:{url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"MADE BY LIYANAARACHCHI AVISHKA THIMIRA LAKSHAN 💚"},{quoted:mek})
+    await conn.sendMessage(from,{audio:audioBuffer,mimetype:"audio/mpeg"},{quoted:mek})
+    await conn.sendMessage(from,{document:audioBuffer,mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"MADE BY LIYANAARACHCHI AVISHKA THIMIRA LAKSHAN 💚"},{quoted:mek})
     
 }catch(e){
 console.log(e)
