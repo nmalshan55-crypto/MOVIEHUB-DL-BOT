33d5e03320d493ae3f2a603cbe5ed7d4ee3217f8
Shan-droid770 <kumaradissanayaka30@gmail.com>
2026-07-16 20:37:55 +0530

    Fix error handling to return instead of break

--- plugins/baiscope.js
+++ plugins/baiscope.js
@@ -21,7 +21,7 @@ async (socket, msg, m, { from, args }) => {
 • .baiscope iron man
 • .bs dark knight\n\n📝 _Please provide the Movie_ _or TV Series name!_${DEFAULT_FOOTER}`
         }, { quoted: msg });
-        break;
+        return;
     }
 
     const query = args.join(' ');
