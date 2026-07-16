7fc3f5b3c73cca65512c9c1accd54042708b3bc2
Shan-droid770 <kumaradissanayaka30@gmail.com>
2026-07-16 20:20:30 +0530

    Refactor GEMINI_API_KEYS assignment logic
    Updated GEMINI_API_KEYS assignment to use a single fallback key.

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ module.exports = {
     MOVIE_API_KEY: process.env.API_KEY || "sky|2483faa7f5630311464123d017fc7acc2aec6da0",
 
     // Gemini AI Keys - Supports multiple keys (recommended for quota limits)
-    GEMINI_API_KEYS: process.env.GEMINI_API_KEYS 
+    /*GEMINI_API_KEYS: process.env.GEMINI_API_KEYS 
         ? process.env.GEMINI_API_KEYS.split(',').map(key => key.trim()).filter(Boolean)
         : (process.env.GEMINI_API_KEY ? [process.env.GEMINI_API_KEY] : [AQ.Ab8RN6LSttakd5WOd674fI-2RNtwWqMKfIl-ERrTT6fr9m3zhw,
                                                                         AQ.Ab8RN6IWRkQJ6C9bDXAlDW2gKx_UDArMzAeLJCWygcuzUZzm0g,
-                                                                       AQ.Ab8RN6I2rdG2ncoq8e1TSNEZrsQtCuZR1DmvOWIQUqWlpDNYCQ]),
+                                                                       AQ.Ab8RN6I2rdG2ncoq8e1TSNEZrsQtCuZR1DmvOWIQUqWlpDNYCQ]),*/
 };
